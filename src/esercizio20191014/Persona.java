df106c44073f8450ab1349c46e1af77b2600f818
LAB-INF <LAB-INF@DESKTOP-F6KS80J>
2019-10-14 12:49:43 +0200

    Implementazione di un costruttore

---
+++ src/esercizio20191014/Persona.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package esercizio20191014;
+
+/**
+ *
+ * @author LAB-INF
+ */
+public class Persona {
+    
+    public String nome;
+    
+    public String cognome;
+    
+    public String titolo;
+    
+    public String professione;
+    
+    public Persona(){
+    
+    
+    nome = " Marco ";
+    
+    cognome = " Rossi ";
+    
+    titolo = " Signore ";
+    
+    professione = " Pizzaiolo ";      
+    
+    }
+    
+    
+    
+    
+    
+}
